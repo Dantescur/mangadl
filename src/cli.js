be1eb88b2df35dc3f864b73a04acd3541e937b10
Dantescur <daniel@thedaniweb.eu.org>
2023-12-28 11:35:40 -0500

    A  .eslintrc.cjs M  package.json M  pnpm-lock.yaml M  src/cli.js D  src/commands/download.js

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 import { program } from "commander";
 import searchCommand from "./commands/search.js";
-import downloadCommand from "./commands/download.js";
 
 program.version("1.0.0").description("A manga downloader CLI for visortmo");
 
 searchCommand(program);
-downloadCommand(program);
-
 program.parse(process.argv);
